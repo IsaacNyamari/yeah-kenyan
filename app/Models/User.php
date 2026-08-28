9244f4b3b7104ba8c6fbd10af4ca1978c1d9e85c
isaacNyamari <jablessions76@gmail.com>
2026-08-28 13:13:23 +0300

    revert nothing

--- app/Models/User.php
+++ app/Models/User.php
@@ -35,7 +35,6 @@ use Spatie\Permission\Traits\HasRoles;
 #[Hidden(['password', 'two_factor_secret', 'two_factor_recovery_codes', 'remember_token'])]
 class User extends Authenticatable implements PasskeyUser
 {
-    // nothing
     /** @use HasFactory<UserFactory> */
     use HasFactory, HasRoles, Notifiable, PasskeyAuthenticatable, TwoFactorAuthenticatable, MustVerifyEmail;
 
